2c47d3c1d7a48b005edd2a2282f2ff2fc6e41057
Douglas Muth <dmuth@dmuth.org>
2015-10-23 23:17:18 -0400

    Added logging

--- bin/get-regional-rail-trainview.sh
+++ bin/get-regional-rail-trainview.sh
@@ -10,6 +10,14 @@
 #
 set -e
 
+
+#
+# Where are we logging our JSON?
+#
+HERE=$(dirname $0)
+LOG="${HERE}/train-log.txt"
+
+
 #
 # Sanity check to see if jq is present.
 #
@@ -24,6 +32,19 @@ then
 	exit 1
 fi
 
+#
+# Check to see if ts is installed
+#
+if test ! $(which ts) 
+then
+	echo "! "
+	echo "! $0: Error! You need the 'ts' utility installed!"
+	echo "! "
+	echo "! This can likely be installed with apt-get or yum, from the 'moreutils' package."
+	echo "! "
+	exit
+fi
+
 
 while true
 do
@@ -35,6 +56,11 @@ do
 	LEN=$(echo $OUTPUT | jq length)
 
 	#
+	# Store our complete output to the log in case we need it later.
+	#
+	echo $OUTPUT | ts >> $LOG
+
+	#
 	# Now loop through those arrays, and print each array on a line.
 	# This makes the data easier to digest in Splunk, as we'll have a 
 	# separate event for each train.
@@ -42,8 +68,12 @@ do
 	END=$(($LEN - 1))
 	for I in $(seq 0 $END)
 	do
+		#
+		# Write to stdout so that Splunk picks it up
+		#
 		echo $OUTPUT | jq .[${I}] | tr -d "\n"
 		echo ""
+
 	done
 
 
