c503bc74ee609cd9ef50b402a14080a3f4b1eeb2
Douglas Muth <dmuth@dmuth.org>
2016-03-08 23:56:48 -0500

    Added docs and cleaned up the logic a bit

--- htdocs/assets/script.js
+++ htdocs/assets/script.js
@@ -10,34 +10,41 @@
 var tryApi = {
 
 	//
-	// Keep me sane
-	//
-	self: this,
-
-	//
 	// Maximum number of tries
 	//
 	maxCount: 5,
 
-	try: function(url, cb, count) {
+	/**
+	* @param string url The URL we are trying to hit
+	* @param object callback Our callback to call when successful
+	* @param integer count How many tries have we made?
+	* @param integer totalMs How many ms have we delayed?
+	*/
+	try: function(url, cb, count, totalMs) {
 
-		url = "http://httpbin.org/status/500"; // Debugging
+		// Keep me sane
+		var self = this;
 
-		count = count || 0;
+		//url = "http://httpbin.org/status/500"; // Debugging
 
-		if (count >= self.maxCount) {
+		count = count || 1;
+		totalMs = totalMs || 0;
+
+		if (count > self.maxCount) {
 			return(null);
 		}
 
+		var apiUrl = url + "?apiCount=" + count + "&apiTotalDelayMs=" + totalMs;
+
 		jQuery.ajax({
-			url: url,
+			url: apiUrl,
 			dataType: "json",
 			success: cb,
 
 		}).fail(function() {
 
 			setTimeout(function() {
-				self.try(url, cb, ++count);
+				self.try(url, cb, ++count, (totalMs + 1000) );
 				}, 1000);
 
 		});
