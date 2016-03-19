85932c1fde80f3e3730fb3a7c6cec434f17ba836
Douglas Muth <dmuth@dmuth.org>
2016-03-19 19:13:20 -0400

    Discovered the "export" endpoint in Splunk, and just made the script MUCH simpler!

--- bin/query-splunk.sh
+++ bin/query-splunk.sh
@@ -23,11 +23,6 @@ URL="https://localhost:8089/services/search/jobs"
 #
 QUERY="search index=septa_analytics earliest=-5m | head limit=10"
 
-#
-# Do we want to export the raw data?
-#
-EXPORT=""
-
 
 if test "$1" == "-h" -o "$1" == "--help"
 then
@@ -35,11 +30,6 @@ then
 	>&2 echo "! Syntax: $0 [--export] query"
 	>&2 echo "! "
 	exit 1
-
-elif test "$1" == "--export"
-then
-	EXPORT=1
-	QUERY=$2
 
 elif test "$1"
 then
@@ -53,73 +43,12 @@ fi
 >&2 echo "# "
 >&2 echo "# 	${QUERY}"
 >&2 echo "# "
-if test "$EXPORT"
-	then
-	>&2 echo "# "
-	>&2 echo "# (The results will be exported in JSON format)"
-	>&2 echo "# "
-fi
 
 
-# xmllint is part of the libxml2-utils package
-RESULT=$(curl -4 -s -u ${UN}:${PW} -k ${URL} -d search="${QUERY}" )
-JOBID=$(echo $RESULT | xmllint --xpath "/response/sid/text()" - || true)
-
-if test ! "${JOBID}"
-then
-	>&2 echo "! "
-	>&2 echo "! No Job ID found! "
-	>&2 echo "! "
-	>&2 echo "! Things to look for:"
-	>&2 echo "! - Are percent signs encoded as '%25'?"
-	>&2 echo "! - Does the query start with 'search'?"
-	>&2 echo "! "
-
-	exit 1
-fi
-
-
->&2 echo "# "
->&2 echo "# Got Job ID: ${JOBID}"
->&2 echo "# "
-
->&2 echo "# Fetching results... "
->&2 echo "# "
-
-
-#
-# Get the state of our job, and keep looping as long as it's not done.
-#
-KEY=".entry[0].content.dispatchState"
-STATE=$(curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID} --get -d output_mode=json | jq ${KEY})
-
-while true
-do
-	if test ${STATE} == '"DONE"'
-	then
-		break
-	fi
-
-	>&2 echo "# State is ${STATE}. Sleeping and trying again..."
-	STATE=$(curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID} --get -d output_mode=json | jq ${KEY})
-	sleep 1
-
-done
-
-
-if test ! "$EXPORT"
-then
-	curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID}/results --get -d "output_mode=json&count=0"
-
-else
-	#
-	# Run our query through some filters so we just get the raw rows
-	#
-	curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID}/results --get -d "output_mode=json&count=0" \
-		| jq .results[]._raw \
-		| sed -e "s/^\"//" -e "s/\"$//" -e 's/\\//g'
-
-fi
+curl -4 -s -u ${UN}:${PW} -k "${URL}/export" -d "search=${QUERY}" -d "output_mode=json" \
+	| jq .result._raw \
+	| sed -e "s/^\"//" -e "s/\"$//" -e 's/\\//g' \
+	| egrep -v "^null$"
 
 
 
