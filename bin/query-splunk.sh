a8bba686ee7abe9d3b1f2bb9574c21de6c46b96e
Douglas Muth <dmuth@dmuth.org>
2016-03-19 18:04:07 -0400

    Added export functionality

--- bin/query-splunk.sh
+++ bin/query-splunk.sh
@@ -13,21 +13,53 @@ set -e
 UN="admin"
 PW="adminpw"
 
+#
+# Our Splunk endpoint
+#
 URL="https://localhost:8089/services/search/jobs"
+
 #
 # Default query, this can be overridden
 #
 QUERY="search index=septa_analytics earliest=-5m | head limit=10"
 
-if test "$1"
+#
+# Do we want to export the raw data?
+#
+EXPORT=""
+
+
+if test "$1" == "-h" -o "$1" == "--help"
+then
+	>&2 echo "! "
+	>&2 echo "! Syntax: $0 [--export] query"
+	>&2 echo "! "
+	exit 1
+
+elif test "$1" == "--export"
+then
+	EXPORT=1
+	QUERY=$2
+
+elif test "$1"
 then
 	QUERY=$1
+
 fi
 
 
 >&2 echo "# " 
->&2 echo "# Executing query: ${QUERY}"
+>&2 echo "# Executing query: "
 >&2 echo "# "
+>&2 echo "# 	${QUERY}"
+>&2 echo "# "
+if test "$EXPORT"
+	then
+	>&2 echo "# "
+	>&2 echo "# (The results will be exported in JSON format)"
+	>&2 echo "# "
+fi
+
 
 # xmllint is part of the libxml2-utils package
 RESULT=$(curl -4 -s -u ${UN}:${PW} -k ${URL} -d search="${QUERY}" )
@@ -74,6 +106,20 @@ do
 
 done
 
-curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID}/results --get -d output_mode=json
+
+if test ! "$EXPORT"
+then
+	curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID}/results --get -d output_mode=json
+
+else
+	#
+	# Run our query through some filters so we just get the raw rows
+	#
+	curl -4 -s -u ${UN}:${PW} -k ${URL}/${JOBID}/results --get -d output_mode=json \
+		| jq .results[]._raw \
+		| sed -e "s/^\"//" -e "s/\"$//" -e 's/\\//g'
+
+fi
+
 
 
