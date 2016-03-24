85b811d5c312c4aa6c9e5a39d389f73ffabf63ff
Douglas Muth <dmuth@dmuth.org>
2016-03-23 23:55:19 -0400

    Added Redis support

--- htdocs/lib/query/stations.class.php
+++ htdocs/lib/query/stations.class.php
@@ -24,25 +24,38 @@ class Stations extends Base {
 	function getStations() {
 
 		$retval = array();
+		$redis_key = "stations";
 
-		$query = 'search index="septa_analytics" earliest=-24h '
-			. '| fields nextstop '
-			. '| sort 0 nextstop '
-			. '| dedup nextstop'
-			;
+		if ($retval = $this->redisGet($redis_key)) {
+			return($retval);
 
-		$retval = $this->query($query);
-		$retval["metadata"]["_comment"] = "A list of all stations";
+		} else {
 
-		$stations = array();
-		foreach ($retval["data"] as $key => $value) {
-			$station = $value["nextstop"];
-			$stations[$station] = $station;
+			$query = 'search index="septa_analytics" earliest=-24h '
+				. '| fields nextstop '
+				//
+				// Zero returns all results, otherwise there is a 
+				// default limit of 10,000. (whyyy?)
+				//
+				. '| sort 0 nextstop '
+				. '| dedup nextstop'
+				;
+
+			$retval = $this->query($query);
+			$retval["metadata"]["_comment"] = "A list of all stations";
+
+			$stations = array();
+			foreach ($retval["data"] as $key => $value) {
+				$station = $value["nextstop"];
+				$stations[$station] = $station;
+			}
+
+			$retval["data"] = $stations;
+			$this->redisSet($redis_key, $retval);
+
+			return($retval);
+
 		}
-
-		$retval["data"] = $stations;
-
-		return($retval);
 
 	} // End of getTrains()
 
